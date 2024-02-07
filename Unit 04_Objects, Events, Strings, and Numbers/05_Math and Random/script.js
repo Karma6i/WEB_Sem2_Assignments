a18d04c744f8eb1f2311f9cf881d376bbbda8caa
Marisa Velazquez <velazmar001@warren.k12.in.us>
2024-02-07 08:58:08 -0500

    4.5.3 Assign

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -1,10 +1,11 @@
-function takeSquareRoot() {
+function findNumSign() {
     let number = +document.getElementById("input1").value;
+
     
 
-    let squareRoot = Math.sqrt(number);
-    document.getElementById("result").innerHTML = squareRoot;
+    let findNumSign = Math.sign(number);
+    document.getElementById("result").innerHTML = Math.sign(number);
     
 }
 
-// Make another function for takeAbsoluteValue
+// I'm pretty sure this is how it's supposed to work but I don't really get why the end result comes out that way
