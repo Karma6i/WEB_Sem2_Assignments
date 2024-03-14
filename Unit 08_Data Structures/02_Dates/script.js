55ad66025638f31bf49cc24eceb982435853e08b
Marisa Velazquez <velazmar001@warren.k12.in.us>
2024-03-14 08:33:04 -0400

    8.3.2 Calendar

--- Unit 08_Data Structures/02_Dates/script.js
+++ Unit 08_Data Structures/02_Dates/script.js
@@ -3,47 +3,46 @@ const month = currentDate.getMonth();
 const year = currentDate.getFullYear();
 switch (month) {
     case 0:
-        document.getElementById("month").innerHTML = "January";
+        document.getElementById("month").innerHTML = "January " + year;
         break;
     case 1:
-        document.getElementById("month").innerHTML = "February";
+        document.getElementById("month").innerHTML = "February " + year;
         break;
     case 2:
-        document.getElementById("month").innerHTML = "March";
+        document.getElementById("month").innerHTML = "March " + year;
         break;
     case 3:
-        document.getElementById("month").innerHTML = "April";
+        document.getElementById("month").innerHTML = "April " + year;
         break;
     case 4:
-        document.getElementById("month").innerHTML = "May";
+        document.getElementById("month").innerHTML = "May " + year;
         break;
     case 5:
-        document.getElementById("month").innerHTML = "June";
+        document.getElementById("month").innerHTML = "June " + year;
         break;
     case 6:
-        document.getElementById("month").innerHTML = "July";
+        document.getElementById("month").innerHTML = "July " + year;
         break;
     case 7:
-        document.getElementById("month").innerHTML = "August";
+        document.getElementById("month").innerHTML = "August "+ year ;
         break;
     case 8:
-        document.getElementById("month").innerHTML = "September";
+        document.getElementById("month").innerHTML = "September " + year ;
         break;
     case 9:
-        document.getElementById("month").innerHTML = "October";
+        document.getElementById("month").innerHTML = "October " + year;
         break;
     case 10:
-        document.getElementById("month").innerHTML = "November";
+        document.getElementById("month").innerHTML = "November " + year;
         break;
     case 11:
-        document.getElementById("month").innerHTML = "December";
+        document.getElementById("month").innerHTML = "December " + year ;
         break;
     default:
-        document.getElementById("month").innerHTML = "Unknown Month";
+        document.getElementById("month").innerHTML = "Unknown Month " + year;
         break;
 }
 
-document.getElementById("month").innerHTML = document.getElementById("month").innerHTML + " " + year;
 
 
 const calendarMap = new Map([
@@ -93,8 +92,11 @@ const calendarMap = new Map([
 
 const firstDayDate = new Date(year, month, 1);
 const firstDayNumber = firstDayDate.getDay() + 1;
+
 const lastDayofMonth = new Date(year, month + 1, 1);
 lastDayofMonth.setDate(lastDayofMonth.getDate() - 1);
+
+
 const lastDayOfMonthNumber = lastDayofMonth.getDate();
 console.log(lastDayOfMonthNumber);
 
